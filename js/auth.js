fa6a1a71c1b171cb612c6f84a620c5506e94d336
tatadata-hub <tatadata2372026@gmail.com>
2026-07-23 14:37:30 +0700

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,47 @@
+// ==========================
+// SHOW PASSWORD
+// ==========================
 
+const togglePassword = document.getElementById("togglePassword");
+
+const password = document.getElementById("password");
+
+togglePassword.addEventListener("click",()=>{
+
+    if(password.type==="password"){
+
+        password.type="text";
+
+        togglePassword.innerHTML="🙈";
+
+    }else{
+
+        password.type="password";
+
+        togglePassword.innerHTML="👁";
+
+    }
+
+});
+
+// ==========================
+// LOGIN
+// ==========================
+
+const loginForm=document.getElementById("loginForm");
+
+loginForm.addEventListener("submit",(e)=>{
+
+    e.preventDefault();
+
+    const email=document.getElementById("email").value;
+
+    const pass=document.getElementById("password").value;
+
+    console.log(email);
+
+    console.log(pass);
+
+    alert("Sebentar lagi login akan dihubungkan ke Google Apps Script 🚀");
+
+});
